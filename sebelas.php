a6608df00c689382c8346c5b2ea87d3cf79b861f
Agilsjt <agilsujito8@gmail.com>
2024-09-20 13:57:06 +0700

    Praktikum 6F - Membuat Objek Pewarisan

--- sebelas.php
+++ sebelas.php
@@ -1,46 +1,6 @@
 <?php
-class Pegawai {
-    public $nip;
-    public $nama;
-    protected $no_hp;
-    public $alamat;
-
-    public function __construct($nip, $nama, $no_hp, $alamat) {
-        $this->nip = $nip;
-        $this->nama = $nama;
-        $this->no_hp = $no_hp;
-        $this->alamat = $alamat;
-    }
-
-    public function cekIn() {
-        return true;
-    }
-
-    public function cekOut() {
-        return true; 
-    }
-
-    public function getNoHp() {
-        return $this->no_hp;
-    }
-
-    public function setNoHp($no_hp) {
-        $this->no_hp = $no_hp;
-    }
-}
-
-class Dosen extends Pegawai {
-    public $nidn;
-
-    public function __construct($nip, $nama, $no_hp, $alamat, $nidn) {
-        parent::__construct($nip, $nama, $no_hp, $alamat);
-        $this->nidn = $nidn;
-    }
-
-    public function mengajar() {
-        echo "{$this->nama} sedang mengajar perkuliahan.\n";
-    }
-}
+require_once 'App/Admin/Pegawai.php';
+use App\Admin\Pegawai;
 
 $dian = new Dosen (198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
 $dian->mengajar();
