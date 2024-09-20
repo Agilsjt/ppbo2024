a0e75f39affe442c7d71d8af8abd0278c01af5ec
Agilsjt <agilsujito8@gmail.com>
2024-09-20 14:34:50 +0700

    Praktikum 6E - Menggunakan Pewarisan

--- sebelas.php
+++ sebelas.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'App/Admin/Pegawai.php';
-use App\Admin\Pegawai;
+require_once 'App/Model/Akademik/Dosen.php';
+use App\Model\Akademik\Dosen;
 
 $dian = new Dosen (198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
 $dian->mengajar();
