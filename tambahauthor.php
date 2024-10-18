77b00f4654718a2974b1431d2c2a3daf17618b1b
Agilsjt <agilsujito8@gmail.com>
2024-10-18 15:08:30 +0700

    Perbaikan praktikum 10B

--- tambahauthor.php
+++ tambahauthor.php
@@ -6,7 +6,7 @@ use App\View;
 require_once 'vendor/autoload.php';
 
 $author = new Author();
-$author->id = 6;
-$author->name = 'Dian Prawira';
+$author->id =11;
+$author->name = 'Agil Sujito';
 $author->description = 'Email: wiraaja@gmail.com';
 View::json($author->save());
